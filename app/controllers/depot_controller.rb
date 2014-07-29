64176c48340d7909ed0a0aeba5a66e4f9b0fb6e8
gunnxma <gunnxma@gmail.com>
2014-07-29 07:30:56 +0000

    change depot condition

--- app/controllers/depot_controller.rb
+++ app/controllers/depot_controller.rb
@@ -2,9 +2,9 @@ class DepotController < ApplicationController
   before_filter :check_power
   def index
     if current_user.id == 1
-      @q = CarInfo.where("status > 0").search(params[:q])
+      @q = CarInfo.where("status > 0 and status < 3").search(params[:q])
     else
-      @q = CarInfo.where("status > 0 and user_id = ?", current_user.id).search(params[:q])
+      @q = CarInfo.where("status > 0 and status < 3 and user_id = ?", current_user.id).search(params[:q])
     end
     if request.format == :xls
       @cars = @q.result.order(addtime: :desc)
